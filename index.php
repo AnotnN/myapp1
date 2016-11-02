cb82c6bbbcbee52c853d07b45ab0c4fd5b6939c9
Anton <newmailreg@mail.ru>
2016-11-02 13:32:15 +0300

    master - test0

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Hello world</title>
     </head>
     <body>
         <?php
