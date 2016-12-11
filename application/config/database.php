bb1ce7896d1d5e6290e37a8876e46d35c24532ab
Anton <newmailreg@mail.ru>
2016-12-11 11:37:57 +0300

    my commit3

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
 	'database' => 'skibase',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => TRUE,
+	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
