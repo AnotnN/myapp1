dbd6e984e3651a692d3e490d7f70266a28ca5209
Anton <newmailreg@mail.ru>
2016-12-11 11:35:35 +0300

    my commit3

--- application/config/database.php
+++ application/config/database.php
@@ -94,15 +94,3 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-
-
-$link = mysql_connect($db['default']['hostname'], $db['default']['username'], $db['default']['password']);
-//if connection is not successful you will see text error
-if (!$link) {
-       die('Could not connect: ' . mysql_error());
-}
-//if connection is successfuly you will see message bellow
-echo 'Connected successfully';
- 
-mysql_close($link);
-
