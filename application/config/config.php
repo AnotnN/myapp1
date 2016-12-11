d3eb3676bad392a6e605c13fa58f07629d5b9d7c
Anton <newmailreg@mail.ru>
2016-12-11 17:55:49 +0300

    my commit

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost:8880/myapp';
-$config['base_url'] = 'http://localhost:8880/myapp';
+$config['base_url'] = 'http://ski-schule.jelastic.regruhosting.ru/';
 
 /*
 |--------------------------------------------------------------------------
